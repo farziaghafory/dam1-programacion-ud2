a4cc1e434658a6e1cbb3120884607bee5b8bfb30
farzia <152214998+farziaghafory@users.noreply.github.com>
2025-10-01 13:55:47 +0200

    Create DatosUsuario.java

---
+++ src/DatosUsuario.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DatosUsuario {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce your name");
+        String nombre = sc.nextLine();
+        System.out.print("Introduce your age: ");
+        int edad;
+        
+    }
+}
+    
