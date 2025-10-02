9dfe8d180aad8ac81cc2684b17058edfe0ec9b67
farzia <ghaforyfarzia@gmail.com>
2025-10-03 00:31:33 +0200

    Update DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,29 +1,44 @@
 import java.util.Scanner;
 import utilidades.ProcesadorTexto;
+import utilidades.Usuario;
 
 public class DatosUsuario {
-
+   //main method, It is where Java starts running the program
     public static void main(String[] args) {
+        //create a Scanner object called sc and listen to the user
         Scanner sc = new Scanner(System.in);
-        System.out.println("introduce your name");
+
+        // ask for name and save it under nombre variable
+        System.out.println("Escribe tu nombre: ");
         String nombre = sc.nextLine();
-        System.out.print("Introduce your age: ");
-        int edad;
-        try {
-            edad = Integer.parseInt(sc.nextLine());
-            if (edad < 0) {
-                System.out.println("La edad no puede ser negativa.");
-                return;
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Por favor, introduce un número valido para la edad.");
-            return;
-        }
-        System.out.println("introduce you email");
+// ask for age and save it under edad variable as an integar
+        System.out.println("Escribe tu edad: ");
+        int edad = sc.nextInt();
+        sc.nextLine(); // What Professor said in class
+
+        System.out.println("Escribe tu email: ");
         String email = sc.nextLine();
+
+        // Call a method esEmailValido (from class ProcesadorTexto).
+        // Result is true/false and stored in emailValido.
         boolean emailValido = ProcesadorTexto.esEmailValido(email);
+
+        if (emailValido) {
+            //Create a new Usuario object with the data you typed (name, age, email).
+           Usuario usuario = new Usuario(nombre, edad , email);
+           // call mostrarinformation method from user class and show the user information
+            usuario.mostrarInformacion();
+        } else {
+            System.out.println("El usuario no puede ser creado porque el email introducido no es valido");
+        }
+
+        // check the age and see if user can drive or not
+        if (edad > 18) {
+            System.out.println("Tienes edad para conducir");
+        } else {
+            System.out.println("No tienes edad para conducir");
+        }
+
+        sc.close();
     }
-
-
 }
-    
