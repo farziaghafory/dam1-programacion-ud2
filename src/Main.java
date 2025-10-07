4df7877e7bf18ed9846fbe89b2362753769b05ed
farzia <152214998+farziaghafory@users.noreply.github.com>
2025-10-07 11:38:27 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ public class Main {
     
         // EJERCICIO 1: Uso de atributos
         
-        // System.out.println(cuenta.saldo); // ← Error: saldo is private
+        // System.out.println(cuenta.saldo); // ← Error: AS sALDO IS PRIVATE
 
         
         // EJERCICIO 2: Métodos públicos
-
+        // 1. Ingresa 100€ usando depositar().
+        // 2. Retira 30€ usando retirar().
+        // 3. Imprime el saldo con getSaldo().
         cuenta.depositar(100);
         cuenta.retirar(30);
         System.out.println("Saldo actual: " + cuenta.getSaldo() + "€");
@@ -29,7 +31,7 @@ public class Main {
 }
 
 class CuentaBancaria {
-    private double saldo;
+    private double saldo;  // EX1: wE HAVE IT HERE IN THE CLASS cUENTAbANCARIA AND IT IS PRIVATE SO WE CAN NOT USE IT IN OTHER CLASSES TOO
 
     public void depositar(double cantidad) {
         if (cantidad > 0) {
@@ -57,15 +59,15 @@ class CuentaBancaria {
 
 
 // RESPUESTAS DE REFLEXIÓN
-// ================================
+
 // Ejercicio 1: ¿por qué no puedo acceder al saldo directamente?
 // Respuesta: ....................................................
 
 // Ejercicio 2: ¿por qué sí puedo usar los métodos depositar(), retirar() y getSaldo()?
-// Respuesta: ....................................................
+// Respuesta: beacuase they are public
 
 // Ejercicio 3: ¿qué significa el error al intentar llamar a registrarOperacion()?
-// Respuesta: ....................................................
+// Respuesta: because it is private and we cant call it from other classes
 
 // Ejercicio 4: 
 // - ¿Qué ventajas tiene que saldo sea private?
