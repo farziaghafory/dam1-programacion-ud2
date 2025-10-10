cafb21bf1b1ad22e0bb0c114e59133645d48e05c
farzia <152214998+farziaghafory@users.noreply.github.com>
2025-10-10 10:44:51 +0200

    he

---
+++ src/GestorTareas.java
@@ -0,0 +1,10 @@
+public class GestorTareas {
+public static void main(String[] args){
+bienvanida();
+}
+public static void bienvanida(){
+    System.out.println("Bienvanida al programa");
+}
+
+
+}
